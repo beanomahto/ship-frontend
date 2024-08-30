c26ad81037482a5d2be2281b3d0c3c46019b22e9
shivam <shivammahrolia@gmail.com>
2024-08-30 19:21:14 +0530

    ij

--- src/hooks/useCreateShipment.js
+++ src/hooks/useCreateShipment.js
@@ -17,7 +17,7 @@ const useCreateShipment = () => {
 
       switch (deliveryPartnerName) {
         case 'Ecom Express':
-          url = 'http://localhost:5000/api/ecomExpress/createShipment';
+          url = 'https://backend.shiphere.in/api/ecomExpress/createShipment';
           log = 'ecom hit';
           break;
         case 'Amazon Shipping':
@@ -26,11 +26,11 @@ const useCreateShipment = () => {
           break;
         case 'Xpressbees':
         //   url = 'https://backend.shiphere.in/api/xpressbees/createShipment';
-          url = 'https://localhost:5000/api/xpressbees/createShipment';
+          url = 'https://backend.shiphere.in/api/xpressbees/createShipment';
           log = 'xpress hit';
           break;
         case 'Delhivery':
-          url = 'http://localhost:5000/api/deliveryOne/create';
+          url = 'https://backend.shiphere.in/api/deliveryOne/create';
           log = 'delhivery hit';
           break;
         case 'Blue Dart':
