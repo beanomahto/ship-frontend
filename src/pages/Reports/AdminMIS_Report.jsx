cb842c9e188a6438506eb846860a43a63abb04b3
shivam <shivammahrolia@gmail.com>
2024-11-21 17:16:40 +0530

    ok

--- src/pages/Reports/AdminMIS_Report.jsx
+++ src/pages/Reports/AdminMIS_Report.jsx
@@ -8,7 +8,7 @@ const AdminMIS_Report = () => {
         setEmail(e.target.value);
     };
 
-    const handleSubmit = async (e) => {
+    const handleMisSubmit = async (e) => {
         e.preventDefault();
 
         const data = {
@@ -45,10 +45,48 @@ const AdminMIS_Report = () => {
         }
     };
 
+    const handleNdrSubmit = async (e) => {
+        e.preventDefault();
+
+        const data = {
+            email,
+        };
+
+        try {
+            const response = await fetch('https://backend.shiphere.in/api/report/generatendr', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: localStorage.getItem('token'),
+                },
+                body: JSON.stringify(data),
+            });
+
+            if (response.ok) {
+                const blob = await response.blob();
+                const url = window.URL.createObjectURL(blob);
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', 'orders_report.xlsx');
+                document.body.appendChild(link);
+                link.click();
+                link.remove();
+                window.URL.revokeObjectURL(url);
+                message.success('MIS Download successfully');
+                setEmail('');
+            } else {
+                message.error('Failed to download MIS');
+            }
+        } catch (error) {
+            message.error('An error occurred while downloading MIS');
+        }
+    };
+
     return (
+        <div>
         <div className='formConsprt'>
-            <form className="formsprt" onSubmit={handleSubmit}>
-                <p className="titlesprt">Download Report</p>
+            <form className="formsprt" onSubmit={handleMisSubmit}>
+                <p className="titlesprt">Download MIS Report</p>
                 <div className='flex2sprt'>
                     <div className="flexsprt">
                         <label className='iptsprt'>
@@ -66,6 +104,27 @@ const AdminMIS_Report = () => {
                 <button className="submitsprt" type="submit">Submit</button>
             </form>
         </div>
+         <div className='formConsprt'>
+         <form className="formsprt" onSubmit={handleNdrSubmit}>
+             <p className="titlesprt">Download NDR Report</p>
+             <div className='flex2sprt'>
+                 <div className="flexsprt">
+                     <label className='iptsprt'>
+                         <span>Email</span>
+                         <input
+                             className="inputsprt"
+                             type="email"
+                             value={email}
+                             onChange={handleEmailChange}
+                             required
+                         />
+                     </label>
+                 </div>
+             </div>
+             <button className="submitsprt" type="submit">Submit</button>
+         </form>
+     </div>
+     </div>
     );
 };
 
