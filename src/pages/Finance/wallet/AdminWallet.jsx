9a3e96cf9913db36225a8a9a9c0e5c2b121b9e96
Deepali <deepalisahu965@gmail.com>
2024-11-26 11:26:04 +0530

    ok

--- src/pages/Finance/wallet/AdminWallet.jsx
+++ src/pages/Finance/wallet/AdminWallet.jsx
@@ -1,18 +1,18 @@
-import React, { useState, useMemo } from 'react';
-import { Button, Table, Input } from 'antd';
-import PaymentModel from './Payment/PaymentModel';
-import { usePaymentUserContext } from '../../../context/PaymentUserContext';
-import moment from 'moment';
-import { Helmet } from 'react-helmet';
-import { CSVLink } from 'react-csv';
-import './wallet.css'
+import React, { useState, useMemo } from "react";
+import { Button, Table, Input, DatePicker } from "antd";
+import PaymentModel from "./Payment/PaymentModel";
+import { usePaymentUserContext } from "../../../context/PaymentUserContext";
+import moment from "moment";
+import { Helmet } from "react-helmet";
+import { CSVLink } from "react-csv";
+import "./wallet.css";
 
 const { Search } = Input;
 
 const AdminWallet = () => {
   const { pUsers } = usePaymentUserContext();
   const [modalVisible, setModalVisible] = useState(false);
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
 
   const showModal = () => setModalVisible(true);
   const closeModal = () => setModalVisible(false);
@@ -21,11 +21,12 @@ const AdminWallet = () => {
   const matchesSearchText = (record, searchText) => {
     const lowercasedText = searchText.toLowerCase();
     return (
-      moment(record.updatedAt).format('DD-MM-YYYY').includes(lowercasedText) ||
-      (record.user?.companyName?.toLowerCase().includes(lowercasedText) || 'N/A'.includes(lowercasedText)) ||
-      (record.credit?.toString().toLowerCase().includes(lowercasedText)) ||
-      (record.amount?.toString().toLowerCase().includes(lowercasedText)) ||
-      (record.remark?.toLowerCase().includes(lowercasedText))
+      moment(record.updatedAt).format("DD-MM-YYYY").includes(lowercasedText) ||
+      record.user?.companyName?.toLowerCase().includes(lowercasedText) ||
+      "N/A".includes(lowercasedText) ||
+      record.credit?.toString().toLowerCase().includes(lowercasedText) ||
+      record.amount?.toString().toLowerCase().includes(lowercasedText) ||
+      record.remark?.toLowerCase().includes(lowercasedText)
     );
   };
 
@@ -36,96 +37,162 @@ const AdminWallet = () => {
 
   // Function to convert data to CSV format
   const generateCsvData = () => {
-    return filteredUsers.map(user => ({
-      'Date & Time': moment(user.updatedAt).format('DD-MM-YYYY'),
-      'Company Info': user.user?.companyName || 'N/A',
-      'Payment': user.credit,
-      'Total': user.amount,
-      'Remark': user.remark,
+    return filteredUsers.map((user) => ({
+      "Date & Time": moment(user.updatedAt).format("DD-MM-YYYY"),
+      "Company Info": user.user?.companyName || "N/A",
+      Payment: user.credit,
+      Total: user.amount,
+      Remark: user.remark,
     }));
   };
 
   const newOrders = [
+    // {
+    //   title: 'Date & Time',
+    //   dataIndex: 'updatedAt',
+    //   render: (text, pUsers) => moment(pUsers?.updatedAt).format('DD-MM-YYYY'),
+    // },
     {
-      title: 'Date & Time',
-      dataIndex: 'updatedAt',
-      render: (text, pUsers) => moment(pUsers?.updatedAt).format('DD-MM-YYYY'),
+      title: "Date & Time",
+      dataIndex: "updatedAt",
+      sorter: (a, b) => moment(a.updatedAt).unix() - moment(b.updatedAt).unix(),
+      filterDropdown: ({
+        setSelectedKeys,
+        selectedKeys,
+        confirm,
+        clearFilters,
+      }) => {
+        const [rangePickerValue, setRangePickerValue] = React.useState(null);
+
+        return (
+          <div style={{ padding: 8 }}>
+            <DatePicker.RangePicker
+              value={rangePickerValue}
+              style={{ marginBottom: 8, display: "block" }}
+              onChange={(dates) => {
+                if (dates) {
+                  const startDate = dates[0].startOf("day").toISOString();
+                  const endDate = dates[1].endOf("day").toISOString();
+                  setSelectedKeys([[startDate, endDate]]);
+                  setRangePickerValue(dates);
+                } else {
+                  setSelectedKeys([]);
+                  setRangePickerValue(null);
+                }
+              }}
+            />
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <Button
+                type="primary"
+                onClick={() => confirm()}
+                size="small"
+                style={{ width: 90 }}
+              >
+                Filter
+              </Button>
+              <Button
+                onClick={() => {
+                  clearFilters();
+                  setRangePickerValue(null);
+                }}
+                size="small"
+                style={{ width: 90 }}
+              >
+                Reset
+              </Button>
+            </div>
+          </div>
+        );
+      },
+      onFilter: (value, record) => {
+        const [startDate, endDate] = value;
+        const orderDate = moment(record.updatedAt).toISOString();
+        return orderDate >= startDate && orderDate <= endDate;
+      },
+      render: (text, pUsers) => (
+        <div>
+          {moment(pUsers?.updatedAt).format("DD-MM-YYYY")}
+          <span style={{ marginLeft: "10px", fontStyle: "italic" }}>
+            {moment(pUsers?.updatedAt).format("HH:mm")}
+          </span>
+        </div>
+      ),
+      className: "centered-row",
     },
     {
-      title: 'Company Info',
-      dataIndex: 'user',
-      render: (user) => user?.companyName || 'N/A',
+      title: "Company Info",
+      dataIndex: "user",
+      render: (user) => user?.companyName || "N/A",
     },
     {
-      title: 'Payment',
-      dataIndex: 'credit',
+      title: "Payment",
+      dataIndex: "credit",
     },
     {
-      title: 'Total',
-      dataIndex: 'amount',
+      title: "Total",
+      dataIndex: "amount",
     },
     {
-      title: 'Remark',
-      dataIndex: 'remark',
+      title: "Remark",
+      dataIndex: "remark",
     },
   ];
 
   const handleClearSearch = () => {
-    setSearchText('');
+    setSearchText("");
   };
 
   return (
     <div className="admin-wallet">
-  <Helmet>
-    <meta charSet='utf-8' />
-    <meta name='keyword' content={""} />
-    <title>Admin Wallet</title>
-  </Helmet>
-  <div className="admin-wallet__header">
-    <div className="admin-wallet__search">
-      <Search
-        placeholder="Search across all fields"
-        value={searchText}
-        onChange={e => setSearchText(e.target.value)}
-        className="admin-wallet__search-input"
-      />
-      <Button onClick={handleClearSearch} className="admin-wallet__clear-button">
-        Clear
-      </Button>
-    </div>
-    <div className="admin-wallet__actions">
-      <Button
-        type="primary"
-        onClick={showModal}
-        className="admin-wallet__payment-button"
-      >
-        Payment
-      </Button>
-      <PaymentModel visible={modalVisible} onClose={closeModal} />
-      {filteredUsers.length > 0 && (
-        <CSVLink
-          data={generateCsvData()}
-          filename={"filtered_users.csv"}
-          className="ant-btn ant-btn-primary"
-        >
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="keyword" content={""} />
+        <title>Admin Wallet</title>
+      </Helmet>
+      <div className="admin-wallet__header">
+        <div className="admin-wallet__search">
+          <Search
+            placeholder="Search across all fields"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            className="admin-wallet__search-input"
+          />
           <Button
-            type="default"
-            className="admin-wallet__download-button"
+            onClick={handleClearSearch}
+            className="admin-wallet__clear-button"
           >
-            Download CSV
+            Clear
           </Button>
-        </CSVLink>
-      )}
+        </div>
+        <div className="admin-wallet__actions">
+          <Button
+            type="primary"
+            onClick={showModal}
+            className="admin-wallet__payment-button"
+          >
+            Payment
+          </Button>
+          <PaymentModel visible={modalVisible} onClose={closeModal} />
+          {filteredUsers.length > 0 && (
+            <CSVLink
+              data={generateCsvData()}
+              filename={"filtered_users.csv"}
+              className="ant-btn ant-btn-primary"
+            >
+              <Button type="default" className="admin-wallet__download-button">
+                Download CSV
+              </Button>
+            </CSVLink>
+          )}
+        </div>
+      </div>
+      <Table
+        className="table"
+        scroll={{ x: 1000, y: 500 }}
+        dataSource={filteredUsers}
+        columns={newOrders}
+      />
     </div>
-  </div>
-  <Table
-    className='table'
-    scroll={{ x: 1000, y: 500 }}
-    dataSource={filteredUsers}
-    columns={newOrders}
-  />
-</div>
-
   );
 };
 
