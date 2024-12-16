31ea8fd4d21004c2aaa788ea5e1760d819f5e1da
Deepali <deepalisahu965@gmail.com>
2024-12-16 18:51:27 +0530

    ok

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -43,7 +43,7 @@ const data = [
     heading: "Order Lookup",
     icon: FaSearch,
     links: [
-      { label: "Customize Track Order", url: "/customize-track-order" },
+      { label: "Customize Track Order", url: "/" },
       { label: "Box Weight", url: "/set_product_weight" },
     ],
   },
