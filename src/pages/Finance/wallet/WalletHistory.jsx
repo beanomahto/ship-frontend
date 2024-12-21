3cf53992d3cb1eafe5f0ba1999023d0c2dda4033
Deepali <deepalisahu965@gmail.com>
2024-12-22 01:50:21 +0530

    wallet history

--- src/pages/Finance/wallet/WalletHistory.jsx
+++ src/pages/Finance/wallet/WalletHistory.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Table } from 'antd';
-import { Helmet } from 'react-helmet';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Button, Table } from "antd";
+import { Helmet } from "react-helmet";
+import axios from "axios";
 // import { usePaymentUserContext } from '../../../context/PaymentUserContext';
-import moment from 'moment';
-import { Link } from 'react-router-dom'
+import moment from "moment";
+import { Link } from "react-router-dom";
 
 const WalletHistory = () => {
   const [transactions, setTransactions] = useState([]);
@@ -12,49 +12,99 @@ const WalletHistory = () => {
 
   const newOrders = [
     {
-      title: 'Payment ID',
-      dataIndex: '_id',
+      title: "Payment ID",
+      dataIndex: "_id",
+      align: "center",
     },
     {
-      title: 'Credit',
-      dataIndex: 'credit',
+      title: "Credit",
+      dataIndex: "credit",
+      align: "center",
     },
     {
-      title: 'Balance',
-      dataIndex: 'balance',
+      title: "Balance",
+      dataIndex: "balance",
+      align: "center",
+      render: (text, transaction) => (
+        <>
+     <div>{transaction.amount?.toFixed(2)}</div>
+          {/* <div style={{ color: "#888" }}>{transaction.phonepetransactionid}</div> */}
+        </>
+      ),
+    },
+    // {
+    //   title: "Transaction Detail",
+    //   dataIndex: "transaction_id",
+    // },
+    // {
+    //   title: "Transaction Status",
+    //   dataIndex: "status",
+    // },
+    {
+      title: "Transaction Detail",
+      dataIndex: "transaction_id",
+      align: "center",
+      render: (text, transaction) => (
+        <>
+          <div >{transaction.phonepeTransactionId}</div>
+          
+        </>
+      ),
     },
     {
-      title: 'Transaction Detail',
-      dataIndex: 'transaction_id',
-    },
+      title: "Transaction Status",
+      dataIndex: "status",
+      align: "center",
+      render: (text, transaction) => (
+        <>
+          <div>{text}</div>
+          <div
+            style={{
+              color: transaction.phonepeTransactionId ? "green" : "red",
+              fontStyle: "italic",
+            }}
+          >
+            {transaction.phonepeTransactionId ? "Success" : "Failure"}
+          </div>
+        </>
+      ),
+  
+    },    
     {
-      title: 'Transaction Status',
-      dataIndex: 'status',
+      title: "Date & Time",
+      dataIndex: "d&t",
+      align: "center",
+      render: (text, transaction) => (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          {moment(transaction?.updatedAt).format("DD-MM-YYYY")}
+        </div>
+      ),
     },
+    
     {
-      title: 'Date & Time',
-      dataIndex: 'd&t',
-      render: (text, transaction) => moment(transaction?.updatedAt).format('DD-MM-YYYY'),
+      title: "Remark",
+      dataIndex: "remark",
+      align: "center",
     },
-    {
-      title: 'Remark',
-      dataIndex: 'remark',
-    }
   ];
 
   useEffect(() => {
     const fetchTransactions = async () => {
       try {
-        const token = localStorage.getItem('token');
-        const response = await axios.get('https://backend.shiphere.in/api/recharge/getUserRecharge', {
-          headers: {
-            Authorization: `${token}`,
-          },
-        });
+        const token = localStorage.getItem("token");
+        const response = await axios.get(
+          "https://backend.shiphere.in/api/recharge/getUserRecharge",
+          {
+            headers: {
+              Authorization: `${token}`,
+            },
+          }
+        );
+        console.log("res ppp", response);
         setTransactions(response.data);
         setLoading(false);
       } catch (error) {
-        console.error('Error fetching transactions:', error);
+        console.error("Error fetching transactions:", error);
         setLoading(false);
       }
     };
@@ -62,17 +112,17 @@ const WalletHistory = () => {
     fetchTransactions();
   }, []);
 
-  //console.log(transactions);
+  console.log(transactions);
 
   return (
     <div>
       <Helmet>
-        <meta charSet='utf-8' />
-        <meta name='keyword' content={''} />
+        <meta charSet="utf-8" />
+        <meta name="keyword" content={""} />
         <title>Wallet History</title>
       </Helmet>
       <Table
-        className='table'
+        className="table"
         scroll={{ x: 1000, y: 500 }}
         columns={newOrders}
         dataSource={transactions}
